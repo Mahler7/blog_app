db5580d79fae698fd00e35dfb878719a3ed05b43
Justin Jungst <jjungstprogramming@gmail.com>
2017-08-19 13:41:30 -0500

    realtime comments

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ Bundler.require(*Rails.groups)
 module BlogApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.action_cable.mount_path = '/cable'
     config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
